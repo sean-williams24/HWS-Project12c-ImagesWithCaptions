1928ab5cd921c2759afc2bed9c6b028c8d53ac4c
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-18 12:51:43 +0100

    Initial Commit

---
+++ Project 12c - Milestone - Images with Captions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 12c - Milestone - Images with Captions
+//
+//  Created by Sean Williams on 18/10/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
